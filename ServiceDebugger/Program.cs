08f6868518401f8284c33a001bea837fc53da1f9
Diogo Teixeira <diogo.andre.teixeira@gmail.com>
2013-05-07 20:35:18 +0100

    Remaing service references added to the ServiceTester project.

--- ServiceDebugger/Program.cs
+++ ServiceDebugger/Program.cs
@@ -12,14 +12,14 @@ namespace ServiceDebugger
         {
             // Instantiates hosts for all needed services.
             ServiceHost notificationServiceHost = new ServiceHost(typeof(Notifications.NotificationService));
-            //ServiceHost projectServiceHost = new ServiceHost(typeof(Projects.ProjectService));
-            //ServiceHost taskboardServiceHost = new ServiceHost(typeof(Taskboards.TaskboardService));
+            ServiceHost projectServiceHost = new ServiceHost(typeof(Projects.ProjectService));
+            ServiceHost taskboardServiceHost = new ServiceHost(typeof(Taskboards.TaskboardService));
             ServiceHost userServiceHost = new ServiceHost(typeof(Users.UserService));
 
             // Runs all the services.
             notificationServiceHost.Open();
-            //projectServiceHost.Open();
-            //taskboardServiceHost.Open();
+            projectServiceHost.Open();
+            taskboardServiceHost.Open();
             userServiceHost.Open();
 
             System.Console.WriteLine("The services are running. Press any key to terminate.");
@@ -28,8 +28,8 @@ namespace ServiceDebugger
 
             // Close all services.
             notificationServiceHost.Close();
-            //projectServiceHost.Close();
-            //taskboardServiceHost.Close();
+            projectServiceHost.Close();
+            taskboardServiceHost.Close();
             userServiceHost.Close();
         }
     }
