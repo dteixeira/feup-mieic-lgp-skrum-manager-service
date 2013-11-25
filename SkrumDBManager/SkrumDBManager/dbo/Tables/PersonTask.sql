e5c9a0054a46e34bedd7862cd68e5cb72747c9c2
Diogo Teixeira <diogo.andre.teixeira@gmail.com>
2013-11-25 16:04:56 +0000

    Some bugs corrected.

--- SkrumDBManager/SkrumDBManager/dbo/Tables/PersonTask.sql
+++ SkrumDBManager/SkrumDBManager/dbo/Tables/PersonTask.sql
@@ -1,10 +1,10 @@
 ﻿CREATE TABLE [dbo].[PersonTask] (
     [TaskID]       INT NOT NULL,
-    [PersonID]     INT NOT NULL,
+    [PersonID]     INT NULL ,
     [SpentTime]         FLOAT NOT NULL,
     [CreationDate] DATETIME NOT NULL, 
     [PersonTaskID] INT NOT NULL IDENTITY, 
-    CONSTRAINT [FK_PersonTask_ToPerson] FOREIGN KEY ([PersonID]) REFERENCES [dbo].[Person] ([PersonID]),
+    CONSTRAINT [FK_PersonTask_ToPerson] FOREIGN KEY ([PersonID]) REFERENCES [dbo].[Person] ([PersonID]) ON DELETE SET NULL,
     CONSTRAINT [FK_PersonTask_ToTask] FOREIGN KEY ([TaskID]) REFERENCES [dbo].[Task] ([TaskID]) ON DELETE CASCADE, 
     CONSTRAINT [PK_PersonTask] PRIMARY KEY ([PersonTaskID])
 );
